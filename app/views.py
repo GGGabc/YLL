5e9e8e74d0c3a61ef35c5782b364b2a71b508f1f
GGGabc <755352046@qq.com>
2022-05-25 17:37:50 +0800

    noting

--- app/views.py
+++ app/views.py
@@ -2,43 +2,49 @@ from django.shortcuts import render
 from app.models import IMG
 from PIL import Image
 import pytesseract
+from django.http import HttpResponse, HttpResponseRedirect
+import os
+
+def delete_dir(root):
+    dirlist = os.listdir(root)   # 返回dir文件夹里的所有文件列表
+    for f in dirlist:
+        filepath = root + '\\' + f    # 路径与文件名拼接成完整的路径
+        print(filepath)
+        if os.path.isdir(filepath):      # 如果该文件是个文件夹
+            delete_dir(filepath)        # 递归调用函数，将该文件夹内的文件删掉
+            os.rmdir(filepath)         # 把文件夹删掉
+        else:
+            os.remove(filepath)      # 如果该文件不是文件夹，直接删除
+    # os.rmdir(root)   # 最后还需要把root删掉
 
 def uploadImg(request):
-    """
-    图片上传
-    :param request:
-    :return:
-    """
-    # 删除数据库的数据
-    IMG.objects.all().delete()
-    # 删除文件夹里面的文件（未完成）
-
     # 如果有上传文件就存入数据库和文件夹中
     if request.method == 'POST':
+        # 删除数据库的数据
+        IMG.objects.all().delete()
+        # 删除文件夹里面的文件
+        root = r'E:\pycharm\YLL\media\img'  # 要删除的文件夹路径
+        delete_dir(root)
+
         new_img = IMG(
             img=request.FILES.get('img'),
             name=request.FILES.get('img').name
         )
         new_img.save()
+        return HttpResponseRedirect('/show')
     return render(request, 'app/uploading.html')
 
 
 def showImg(request):
-    """
-    图片显示
-    :param request:
-    :return:
-    """
     # 获取文件夹的图片
     imgs = IMG.objects.all()
     content = {
         'imgs': imgs,
     }
     for i in imgs:
-        print(i.img.url)
-    # 进行图像处理将处理出来的文字显示出来
-    pytesseract.pytesseract.tesseract_cmd = 'E://OCR/Tesseract-OCR/tesseract.exe'
-    text = pytesseract.image_to_string(Image.open('E://pycharm/YLL/media/img/2.jpg'), lang='chi_sim')
-    print(text)
+        image = 'E://pycharm/YLL/'+ i.img.url
+        # 图像处理PIL 文字识别OCR
+        pytesseract.pytesseract.tesseract_cmd = 'E://OCR/Tesseract-OCR/tesseract.exe'
+        text = pytesseract.image_to_string(Image.open(image), lang='chi_sim')
     content['hello'] = text
     return render(request, 'app/showing.html', content)
